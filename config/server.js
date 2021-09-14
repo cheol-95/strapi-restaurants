ede5cbf390a2b49c53ef4f657669e672823fa705
cheol-95 <rkdcjf0122@gmail.com>
2021-09-15 00:23:23 +0900

    컬렉션 생성 및 릴레이션 적용

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7729afad18cf843bfb388793c3c1fe5f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6dd93145-d894-4587-860b-03590bc2c206'
+};
